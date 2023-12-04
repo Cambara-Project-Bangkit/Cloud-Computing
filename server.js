2d8a29637e3bde8d338d92d15431139cf3c02ab9
YYanti00 <lindayanti928@gmail.com>
2023-12-05 04:07:38 +0700

    feat: add upload image prediction and connect to flask

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./app/routes/userRoutes');
 const aksaraRoutes = require('./app/routes/aksaraRoutes');
+const predictionRoutes = require('./app/routes/predictionRoutes');
 
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
@@ -25,5 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/aksara', aksaraRoutes);
+app.use('/api/model', predictionRoutes);
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server started on port ${port}`));
